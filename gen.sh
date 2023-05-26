a6f15243bff2dbdc40de573fc0d9d999ad690dfb
hello <keye.myria@gmail.com>
2023-05-26 11:14:27 +0800

    init

--- gen.sh
+++ gen.sh
@@ -1,3 +1,3 @@
 
-openapi-generator generate -i spec/openapi.v0.0.39.json  -g cpp-restsdk --package-name slurmrestapi -o ./
+openapi-generator generate -i spec/openapi.v0.0.37.json  -g cpp-restsdk --package-name slurmrestapi -o ./
 
