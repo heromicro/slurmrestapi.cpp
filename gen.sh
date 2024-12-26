df6ef60bb3a58ed76b669fa1135d37d2e7762536
nono <no@no.com>
2024-12-26 17:12:41 +0800

    slurmrest api for slurm 24.05.5

--- gen.sh
+++ gen.sh
@@ -1,3 +1,2 @@
 
-openapi-generator generate -i spec/openapi.v0.0.xx.json  -g cpp-restsdk --package-name slurmrestapi -o ./
-
+openapi-generator generate -i spec/openapi.24.05.5.json  -g cpp-restsdk --package-name slurmrestapi -o ./
