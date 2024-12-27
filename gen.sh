43d12455a8f6ace337e87d120d467511012ca08e
nono <no@no.com>
2024-12-27 11:54:48 +0800

    gen bash

--- gen.sh
+++ gen.sh
@@ -1,2 +1,2 @@
 
-openapi-generator generate -i spec/openapi.24.05.5.json  -g cpp-restsdk --package-name slurmrestapi -o ./
+openapi-generator generate -i spec/openapi.24.05.5.json  -g cpp-restsdk --additional-properties modelPackage=hm.slurm.client.model,packageName=slurmrestapi --package-name slurmrestapi -o ./
